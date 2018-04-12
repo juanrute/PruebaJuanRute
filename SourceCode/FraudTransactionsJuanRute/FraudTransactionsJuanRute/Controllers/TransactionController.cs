81f08562d2cabfe3b0183e918c99be5ee860a7bc
juanrute <38298315+juanrute@users.noreply.github.com>
2018-04-11 20:52:23 -0500

    First Sprint
    I have created the data base and implement a dll as a data access layer preventing application coupling, added architectural document

---
+++ SourceCode/FraudTransactionsJuanRute/FraudTransactionsJuanRute/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using Model.EntityCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FraudTransactionsJuanRute.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            Facade fc = new Facade();            
+            return View(fc.GetAllTransactions());
+        }
+
+        public ActionResult CreateTX() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTX(Transaction t)
+        {
+            Facade fc = new Facade();
+            fc.SaveTransaction(t);
+            return View();
+        }
+    }
+}
---
+++ SourceCode/FraudTransactionsJuanRute/Model/IFacade.cs
@@ -0,0 +1,18 @@
+﻿using Model.EntityCode;
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public interface IFacade
+    {
+        List<Transaction> GetAllTransactions();
+        string GetTxType(int id);
+        List<TransactionType> GetAllTxType();
+        int SaveTransaction(Transaction t);
+        int ChangeTransactionState(Transaction t);
+        List<Transaction> SearchTxByNameDest(string name);
+        List<Transaction> SearchIsFraudTx();
+        List<Transaction> SearchByTxDate(DateTime dFrom, DateTime dTo);
+    }
+}
